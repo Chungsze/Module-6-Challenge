ee369df2c2928388249da7d98e5262a73158477c
Chungsze <ukrenthouse@gmail.com>
2023-01-24 06:28:14 +0900

    update logic and questions js files

---
+++ assets/js/logic.js
@@ -0,0 +1,111 @@
+//variable to keep track of the quiz state
+let currentQuestionIndex = 0;
+let time = questions.length *15;
+let timerID;
+
+//HTML elements
+let questionsElement = document.getElementById("questions");
+let timerElement = document.getElementById("time");
+let choicesElement = document.getElementById("choices");
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialElement= document.getElementById("initials");
+let feedbackElement = document.getElementById("feedback");
+
+let sfxRight = new Audio("assets/sfx/correct.wav");
+let sfxWrong = new Audio("assets/sfx/incorrect.wav");
+
+function questionClick(){
+  alert("You clicked");
+}
+
+function getQuestions(){
+    let currentQuestion = questions[currentQuestionIndex];
+
+    let titleElement = document.getElementById("question-title");
+
+    titleElement.textContent = currentQuestion.title;
+    choicesElement.innerHTML= "";
+    currentQuestion.choices.forEach(function(choice,index){
+        let choiceButton = document.createElement("button");
+
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+
+        choiceButton.textContent = ` ${index + 1}.${choice}`;
+
+        choiceButton.addEventListener("click", questionClick);
+
+        choicesElement.append(choiceButton); 
+
+
+
+    })
+
+
+    
+}
+
+
+
+function clockTick(){
+
+    time--
+    timerElement.textContent = time;
+
+    if(time <= 0){
+        quizEnd();
+    }
+
+}
+
+function startQuiz(){
+    //alert("Start");
+    let startScreenElement = document.getElementById("start-screen");
+    startScreenElement.setAttribute("class", "hide");
+
+    questionsElement.removeAttribute("class");
+
+    timerID = setInterval(clockTick, 1000)
+
+    timerElement.textContent = time;
+
+    getQuestions();
+
+}
+
+function quizEnd(){
+    clearInterval(timerID);
+
+    let endScreenElement = document.getElementById("end-screen"); 
+    endScreenElement.removeAttribute("class");
+
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    questionsElement.setAttribute("class", "hide");
+
+
+
+}
+
+
+
+function saveHighScore(){
+
+
+}
+
+function checkForEnter(event){
+
+}
+
+startButton.addEventListener("click",startQuiz);
+
+submitButton.addEventListener("click",saveHighScore);
+
+initialElement.addEventListener("keyup",checkForEnter);
+
+
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+let questions =[
+  {
+    "title": " Which type of JavaScript language is ?",
+    "choices": ["Object-Oriented","Object-Based","Assembly-language","High-level"],
+    "answers": "Object-Based"
+  },
+
+  {
+
+    "title":  "When interpreter encounters an empty statements, what it will do:",
+    "choices": ["Shows a warning","Prompts to complete the statement","Throws an error","Ignores the statements"],
+    "answers": "Ignores the statements"
+
+  },
+
+  {
+    "title": " In JavaScript, what is a block of statement ?",
+    "choices": ["Conditional block","block that combines a number of statements into a single compound statement",
+           "both conditional block and a single statement","block that contains a single statement"],
+    "answers": "block that combines a number of statements into a single compound statement"
+
+  }
+
+]    
