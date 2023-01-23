5c4341a6115c6cd4788e22296b40e318acc98556
Chungsze <ukrenthouse@gmail.com>
2023-01-24 03:20:03 +0900

    add all starting files for the assigmnent

---
+++ assets/js/scores.js
@@ -0,0 +1,12 @@
+function printHighScores(){
+
+}
+
+function clearHighScores(){
+
+}
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+printHighScores();
