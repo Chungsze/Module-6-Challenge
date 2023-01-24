81715704f551071caf4c9e25d38b57c399591067
Chungsze <ukrenthouse@gmail.com>
2023-01-24 21:25:32 +0900

    finish the scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,10 +1,29 @@
 function printHighScores(){
     //TODO
-    
+    let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    highScores.sort(function(a,b){
+        return a.score - b.score;
+    })
+
+    highScores.forEach(function(score){
+
+        let li = document.createElement("li");
+        li.textContent = `${score.initials}-${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+
+
+    })
+
 
 }
 
 function clearHighScores(){
+    localStorage.removeItem("highscores");
+    window.location.reload();
+    //TODO
 
 }
 
